015303ab1ddfce167b148658d04251c7641b881b
JQuay <112822945+JQuay@users.noreply.github.com>
2025-10-07 15:44:19 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,41 +1,58 @@
-provider "google" {
-  project     = "euphoric-fusion-454304-v6"
-  region      = "us-central1"
-}
-
-############
-###########
 terraform {
-  required_version = ">= 0.12"
-   backend "gcs" {
-    bucket  = "my_first_cp_bucket"
-    prefix  = "githubactions/state"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.0"
+    }
   }
+
+  required_version = ">= 1.5.0"
 }
 
+provider "google" {
+  project = var.project_id
+  region  = var.region
+  zone    = var.zone
+}
 
+# Variables (inline)
+variable "project_id" {}
+variable "region" { default = "us-central1" }
+variable "zone" { default = "us-central1-a" }
+variable "vm_name" { default = "simple-vm" }
+variable "machine_type" { default = "e2-micro" }
+variable "ssh_user" { default = "terraform-user" }
+variable "public_key_path" { default = "~/.ssh/id_rsa.pub" }
 
-resource "google_compute_instance" "vm_instance" {
-  name         = "my-vm"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+# VM resource
+resource "google_compute_instance" "vm" {
+  name         = var.vm_name
+  machine_type = var.machine_type
+  zone         = var.zone
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-11"
+      image = "debian-cloud/debian-12"
     }
   }
 
   network_interface {
     network = "default"
-    access_config {
-      // This assigns a public IP
-    }
+    access_config {} # ephemeral public IP
   }
 
   metadata = {
-    ssh-keys = "your-username:ssh-rsa YOUR_SSH_PUBLIC_KEY"
+    ssh-keys = "${var.ssh_user}:${file(var.public_key_path)}"
   }
+
+  tags = ["terraform-simple"]
 }
 
-# added a comment 
+# Outputs
+output "public_ip" {
+  value = google_compute_instance.vm.network_interface[0].access_config[0].nat_ip
+}
+
+output "ssh_command" {
+  value = "ssh ${var.ssh_user}@${google_compute_instance.vm.network_interface[0].access_config[0].nat_ip}"
+}
